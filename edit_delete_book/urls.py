10fca974381994ecc78a3c3e6b96a704a99e96b7
arjunajaa <aarjunaja@gmail.com>
2023-10-29 19:03:15 +0700

    implementasi fitur edit book

--- edit_delete_book/urls.py
+++ edit_delete_book/urls.py
@@ -6,4 +6,6 @@ app_name = 'edit_delete_book'
 urlpatterns = [
     path('show/<int:book_id>/', show, name='show'),
     path('edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete/<int:id>/', delete_book, name='delete_book'),
+    path('get_book/<int:id>/', get_book, name='get_book'),
 ]
